b375848a8ce80e8adf36ecf758b06dc3a13d3948
Adrian Valenzuela <adrian.valenzuela@globant.com>
2019-07-26 20:05:13 -0300

    Introduciendo Firebase
    primeros pasitos con firebase

--- app/src/main/java/com/example/adriancloud/login/LoginActivity.kt
+++ app/src/main/java/com/example/adriancloud/login/LoginActivity.kt
@@ -7,20 +7,31 @@ import android.widget.EditText
 import android.widget.ProgressBar
 import com.example.adriancloud.R
 import com.example.adriancloud.utils.show
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    //Firebase Connection
+    private lateinit var auth: FirebaseAuth
+
+    //Components
+    private val editUsername = findViewById<EditText>(R.id.edit_username)
+    private val editPassword = findViewById<EditText>(R.id.edit_password)
+    private val bttnLogin = findViewById<Button>(R.id.bttn_login)
+    private val progressBar = findViewById<ProgressBar>(R.id.login_progress)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val editUsername = findViewById<EditText>(R.id.edit_username)
-        val editPassword = findViewById<EditText>(R.id.edit_password)
-        val bttnLogin = findViewById<Button>(R.id.bttn_login)
-        val progressBar = findViewById<ProgressBar>(R.id.login_progress)
-
+        // Show Keybouard to after 0.6 secs
         show(editUsername, this, 600)
 
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
     }
+
+
 
 }
