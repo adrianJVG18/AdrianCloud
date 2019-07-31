a21c98329807dcd5a93156ab6566a119524f3198
Adrian Valenzuela <adrian.valenzuela@globant.com>
2019-07-31 19:32:00 -0300

    Fixed some bugs in the Login
    Not valid fields were not showing its error messaged

--- app/src/main/java/com/example/adriancloud/login/LoginActivity.kt
+++ app/src/main/java/com/example/adriancloud/login/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.adriancloud.login
 
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
@@ -11,7 +13,9 @@ import com.example.adriancloud.R
 import com.example.adriancloud.utils.show
 import com.google.firebase.auth.FirebaseAuth
 import android.view.MotionEvent
+import android.view.View
 import android.widget.Toast
+import com.example.adriancloud.home.HomeActivity
 import com.google.firebase.auth.FirebaseUser
 
 
@@ -19,12 +23,6 @@ class LoginActivity : AppCompatActivity() {
 
     //Firebase Connection
     private lateinit var auth: FirebaseAuth
-
-    //Components
-    private lateinit var editEmail: EditText
-    private lateinit var editPassword: EditText
-    private lateinit var bttnLogin: Button
-    private lateinit var progressBar: ProgressBar
 
     //Codes
     private val VALID_TEXT = 1
@@ -40,8 +38,8 @@ class LoginActivity : AppCompatActivity() {
         // Initialize Firebase Auth
         auth = FirebaseAuth.getInstance()
 
-        //Obtener referencias de los componentes
-        activateComponents()
+        val editEmail = findViewById<EditText>(R.id.edit_email)
+        activateLoginButton()
 
         // Show Keybouard to after 0.6 secs
         show(editEmail, this, 600)
@@ -58,16 +56,20 @@ class LoginActivity : AppCompatActivity() {
         return super.dispatchTouchEvent(ev)
     }
 
-    fun activateComponents() {
-        editEmail = findViewById<EditText>(R.id.edit_username)
-        editPassword = findViewById<EditText>(R.id.edit_password)
-        bttnLogin = findViewById<Button>(R.id.bttn_login)
+    fun activateLoginButton() {
+        val bttnLogin = findViewById<Button>(R.id.bttn_login)
         bttnLogin.setOnClickListener {
-            val email = editEmail.text.toString()
-            val pass = editPassword.text.toString()
+            val progressBar = findViewById<ProgressBar>(R.id.login_progress)
+            progressBar.visibility = View.VISIBLE
 
-            val isEmailValid = checkIfTextFilledCorrectly(email, 6, 40)
-            val isPassValid = checkIfTextFilledCorrectly(pass, 4, 20)
+            val editEmail = findViewById<EditText>(R.id.edit_email)
+            val editPass = findViewById<EditText>(R.id.edit_password)
+
+            val email = editEmail.text.toString()
+            val pass = editPass.text.toString()
+
+            val isEmailValid = checkIfTextFilledCorrectly("Email", email, 6, 40)
+            val isPassValid = checkIfTextFilledCorrectly("Passowrd", pass, 4, 20)
 
             if (isEmailValid == VALID_TEXT && isPassValid == VALID_TEXT) {
                 showMessage("Attempting to Login")
@@ -76,47 +78,49 @@ class LoginActivity : AppCompatActivity() {
                         if (task.isSuccessful) {
                             // Sign in success, update UI with the signed-in user's information
                             val user = auth.currentUser
+                            completeLogin(user)
 
                         } else {
                             // If sign in fails, display a message to the user.
                             showMessage("Not registered email or wrong password")
                         }
                     }
-            } else {
-                showMessageError("Email", isPassValid)
-                showMessageError("Password", isPassValid)
             }
+            progressBar.visibility = View.GONE
         }
 
-        progressBar = findViewById<ProgressBar>(R.id.login_progress)
     }
 
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
+        if (currentUser!=null){
+            completeLogin(currentUser)
+        }
     }
 
-    private fun checkIfTextFilledCorrectly(text: String, min: Int, max: Int): Int {
-        if (text.isEmpty()) return EMPTY_TEXT
-        if (text.length < min) return TOO_SHORT_TEXT
-        if (text.length > max) return TOO_LONG_TEXT
+    private fun checkIfTextFilledCorrectly(field: String, text: String, min: Int, max: Int): Int {
+        if (text.isEmpty()) return showError(field, EMPTY_TEXT)
+        if (text.length < min) return showError(field, TOO_SHORT_TEXT)
+        if (text.length > max) return showError(field, TOO_LONG_TEXT)
         return VALID_TEXT
     }
-
-    private fun showMessageError(field: String, code: Int) {
+    private fun showError(field: String, code: Int): Int {
         when (code) {
-            EMPTY_TEXT -> showMessage(field + " is empty")
-            TOO_LONG_TEXT -> showMessage(field + " is too long")
-            TOO_SHORT_TEXT -> showMessage(field + "is too short")
+            EMPTY_TEXT -> showMessage("$field is empty")
+            TOO_LONG_TEXT -> showMessage("$field is too long")
+            TOO_SHORT_TEXT -> showMessage("$field is too short")
         }
+        return code
     }
 
     private fun showMessage(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
-    private fun completeLogin(user: FirebaseUser){
-
+    private fun completeLogin(user: FirebaseUser?){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
